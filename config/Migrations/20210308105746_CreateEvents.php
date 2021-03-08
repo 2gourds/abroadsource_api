50b22ad1b8164202347cd04fdaa722fca03b6325
2gourds <arben.garcia@gmail.com>
2021-03-08 19:59:44 +0900

    Added migration files.

---
+++ config/Migrations/20210308105746_CreateEvents.php
@@ -0,0 +1,45 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateEvents extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('events');
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('frequency', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('start_date_time', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('end_date_time', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('duration', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => true,
+        ]);
+        $table->addColumn('', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
